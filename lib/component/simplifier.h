5b1c4a90d1ff444477f477582a19bc72bd73fe31
Firedog01 <jan.rubacha@onet.pl>
2021-11-23 10:42:19 +0100

    w trakcie group pows

--- lib/component/simplifier.h
+++ lib/component/simplifier.h
@@ -20,19 +20,34 @@ class simplifier
 	const node_type type_order[5] {
 		Int, Constant, Variable, Embedded, Function
 	};
-    
+	// default operation order: min, none, div, pow
+
+	// p - n plus node, from 1 to n;
+	// m - n mul node, from 0 to n-1;
+	void get_mul_root(node_ptr& active_mul, int p);
+	void get_node(node_ptr& node, node_ptr& root, int p, int m);
+	int get_n_plus(node_ptr root);
+	int get_n_mul(node_ptr active_plus);
+    //set_prev_node
 	void handle_prev_nodes(node_ptr& ptr1, node_ptr& ptr2);
+	
 	void swap_nodes(node_ptr& ptr1, node_ptr& ptr2, node_ptr& root); 
 	int get_order_type(node_ptr node);
 	int get_order_oper(node_ptr node);
+
     void sort_mul_branches(node_ptr& root);
 
-	void group_ints_pow(node_ptr& root); 
+	void group_pows(node_ptr& root); 
 	void group_ints_div(node_ptr& root); 
 public:
 	void simplify_all(node_ptr& root);
 
 	// friend class math_node;
 };
-
+/*
+jak iterować po wszystkich elementach 
+funkcje: 
+początek mul_branch dla danego i_plus
+następny mul_node - jeśli następny poza zakresem to zwróć początek
+*/
 #endif
--- src/component/simplifier.cpp
+++ src/component/simplifier.cpp
@@ -2,6 +2,49 @@
 
 using namespace std;
 
+void simplifier::get_mul_root(node_ptr& active_mul, int p) {
+	//update root position
+	active_mul = active_mul->get_root();
+	for(int i = 0; i < p; i++) {
+		active_mul = active_mul->get_plus_node();
+	}
+}
+
+void simplifier::get_node(node_ptr& node, node_ptr& root, int p, int m) {
+	get_mul_root(node, p); //node równy szczytowi
+	int n_mul = get_n_mul(node);
+	if(m > n_mul) { // ? >=
+		node = nullptr;
+	} else {
+		for(int i = 0; i < m; i++) {
+			node = node->get_mul_node();
+		}
+	}
+}
+
+int simplifier::get_n_plus(node_ptr root) {
+	int n = 1;
+	
+	while(root->get_plus_node() != nullptr) {
+		n++;
+		root = root->get_plus_node();
+	}
+	// cout << "n_plus_branches = " << n << endl;
+	return n;
+}
+
+int simplifier::get_n_mul(node_ptr active_plus) {
+	int n = 0; //nie dokońca wiem dlaczego, ale inaczej nie działa
+
+	while(active_plus->get_mul_node() != nullptr) {
+		n++;
+		active_plus = active_plus->get_mul_node();
+	}
+	// cout << "n_mul = " << n << endl;
+	return n;
+}
+
+// gets previous from ptr1 node and swaps ptr1 with ptr2
 void simplifier::handle_prev_nodes(node_ptr& ptr1, node_ptr& ptr2) {
 	node_ptr prev = ptr1->get_prev_node();
 	if(prev != nullptr) {
@@ -71,67 +114,29 @@ int simplifier::get_order_oper(node_ptr node) {
 }
 
 void simplifier::sort_mul_branches(node_ptr& root) {
+	node_ptr active = root;
 	
-	// default operation order: min, none, div, pow
-	node_ptr active_plus = root;
-	
-	int n_plus_branches = 1;
-	while(active_plus->get_plus_node() != nullptr) {
-		n_plus_branches++;
-		active_plus = active_plus->get_plus_node();
-	}
-	cout << "n_plus_branches = " << n_plus_branches << endl;
-	
-	for(int i = 0; i < n_plus_branches; i++) {
-		cout << "plus_branch\n";
+	for(int i_plus = 0; i_plus < get_n_plus(root); i_plus++) {
 		bool changed = true;
-		
-		int n_mul = 0;
-		node_ptr active_mul = active_plus;
-		while(active_mul->get_mul_node() != nullptr) {
-			n_mul++;
-			active_mul = active_mul->get_mul_node();
-		}
-		cout << "n_mul = " << n_mul << "\n";
-
 		while(changed) {
 			changed = false;
-			
-			for(int j = 0; j < n_mul; j++) {
-				cout << "i: " << i << " j: " << j << endl;
+			for(int i_mul = 0; i_mul < get_n_mul(root); i_mul++) { 
 
-				cout << "przed\n";
-				active_plus = active_plus->get_root();
-				cout << "po\n";
-				for(int k = 0; k < i; k++) {
-					active_plus = active_plus->get_plus_node();
-				}
-
-				active_mul = active_plus;
-				for(int k = 0; k < j; k++) {
-					active_mul = active_mul->get_mul_node();
-				}
-
-				if(active_mul == nullptr) {
-					cout << "mul null!\n";
-				} else {
-					cout << "mul not null\n";
-				}
-				node_ptr next_node = active_mul->get_mul_node();
+				get_node(active, root, i_plus, i_mul);
+				node_ptr next_node = active->get_mul_node();
 				
-				int act_type = get_order_type(active_mul);
+				int act_type = get_order_type(active);
 				cout << "act_type: " << act_type << endl;
 				int next_type = get_order_type(next_node);
 				cout << "next_type: " << next_type << endl;
 
-				int act_ops = get_order_oper(active_mul);
+				int act_ops = get_order_oper(active);
 				cout << "act_ops: " << act_ops << endl;
 				int next_ops = get_order_oper(next_node);
 				cout << "next_ops: " << next_ops << endl;
 
 				if(act_type > next_type || act_ops > next_ops) {
-					cout << "pre_swap\n";
-					swap_nodes(active_mul, next_node, root);
+					swap_nodes(active, next_node, root);
 					changed = true;
 					cout << "swapped\n";
 				}
@@ -141,49 +146,46 @@ void simplifier::sort_mul_branches(node_ptr& root) {
 	}
 }
 
-void simplifier::group_ints_pow(node_ptr& root) {
-	node_ptr active_plus = root;
-	node_ptr prev_active_mul = nullptr;
-	while(active_plus->get_plus_node() != nullptr) {
-		node_ptr active_mul = active_plus->get_mul_node();
-		while(active_mul->get_mul_node() != nullptr) {
-
-			if(active_mul->get_type() == Embedded) {
-				embedded_ptr ptr = static_pointer_cast<embedded_node>(active_mul);
-				// group_ints_pow(ptr->get_cont());
-			}
-			node_ptr next_active_mul = active_mul->get_mul_node();
-			if(active_mul->get_type() == Int && 
-					next_active_mul->get_type() == Int) {
-				//grupowanie potęg 
-				if(next_active_mul->is_pow()) {
-					node_ptr next_next_mul = next_active_mul->get_mul_node();
-					//next_active_mul->set_mul_node(nullptr);
-					//przestawić poprzedni na new_node
-					//przestwaić flagi z mul_node na new_node;
-
-					//node_ptr x, bool min, bool div, bool pow
-					// embedded_ptr new_node = make_shared<embedded_node>(mul_node, );
-					// new_node->set_mul_node(next_next_mul);
+//math_node->set_pair(true);
+void simplifier::group_pows(node_ptr& root) {
+	node_ptr active = root;
+	
+	for(int i_plus = 0; i_plus < get_n_plus(root); i_plus++) {
+		bool changed = true;
+		while(changed) {
+			changed = false;
+			for(int i_mul = 0; i_mul < get_n_mul(root); i_mul++) { 
+				get_node(active, root, i_plus, i_mul);
+				node_ptr next_node = active->get_mul_node();
+				if(next_node->is_pow()) {
+					node_ptr cont = active;
+					embedded_ptr new_node = make_shared<embedded_node>(
+						cont,
+						active->is_min(),
+						active->is_div(),
+						active->is_pow()
+					);
+					node_ptr new_node_base = static_cast<node_ptr>(new_node);
+					handle_prev_nodes(active, new_node_base);
+					node_ptr next_next_node = next_node->get_mul_node();
+					if(next_next_node != nullptr) {
+						//move new_node->next_mul to next_next_node
+					}
+				}
+				if(active->get_type() == Embedded) {
+					embedded_ptr ptr = static_pointer_cast<embedded_node>(active);
+					node_ptr cont = ptr->get_cont();
+					group_pows(cont);
 				}
 			}
-			prev_active_mul = active_mul;
-			active_mul = active_mul->get_mul_node();
 		}
-		prev_active_mul = nullptr;
-		active_plus = active_plus->get_plus_node();
 	}
 }
 
 void simplifier::group_ints_div(node_ptr& root) {}
 
 void simplifier::simplify_all(node_ptr& root) {
-	// group_ints_pow(root);
-	// sort_mul_branches(root);
-	// group_ints_div(root);
-
-	node_ptr temp = root->get_mul_node();
-	// temp->set_flag(true, 0);
-	// swap_nodes(root, temp, root);
+	group_pows(root);
 	sort_mul_branches(root);
+	// group_ints_div(root);
 }
