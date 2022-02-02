73952ddac5dffab963451be412ced9c13d809121
Firedog01 <jan.rubacha@onet.pl>
2022-02-02 21:46:17 +0100

    implemented help

--- main.cpp
+++ main.cpp
@@ -4,20 +4,25 @@
 #include "lib/calculator.h"
 #include "windows.h"
 
-using namespace calculator;
+using namespace std;
+
+void display_help();
 
 int main(int argc, char** argv) {
 	std::string default_text = "1(2-4)/3*9^6/5*-4";
 	
-	std::cout << "Command line calculator program\n"
-			  << "Made by Jan Rubacha\n"
-	          << "Version: 0.0.2\n"
-			  << "Default input: " << default_text << "\n";
+	cout << "Command line calculator program\n"
+         << "Made by Jan Rubacha\n"
+         << "Version: 0.0.2\n"
+         << "\n"
+         << "Default input: " << default_text << "\n"
+         << "For help type h\n";
+
 	
 	if(argc == 1) {
-		calc calc;
-		std::string input;
-		std::string equation;
+		calculator::calc calc;
+		string input;
+		string equation;
 		
 		
 		while(true) {
@@ -29,12 +34,10 @@ int main(int argc, char** argv) {
 			} else if(input == "d") {
 				equation = default_text;
 			} else if(input == "h") {
-                while(true) {
-                    std::cout << "There is no help.\n";
-                }
+                display_help();
+                continue;
             } else {
-                    equation = input;
-                }
+                equation = input;
             }
 			
 			try {
@@ -50,6 +53,45 @@ int main(int argc, char** argv) {
 		}
 	}
 	return 0;
+}
+
+void display_help() {
+
+    cout << "d  - default input (for debugging purposes)\n"
+         << "h  - display help\n"
+         << "q  - quit program\n"
+         << "\n"
+
+         << "Input different than those above will be parsed\n"
+         << "That is, just type calculation you want to do\n"
+         << "\n"
+
+         << "Available operators:\n"
+         << "\t\'+\'  addition\n"
+         << "\t\'-\'  subtraction\n"
+         << "\t\'*\'  multiplication\n"
+         << "\t\'/\'  division\n"
+         << "\t\'^\'  power\n"
+         << "\t\'^/\' root\n"
+         << "\n"
+
+         << "Rules of syntax:\n"
+         << "\t- every constant needs to be preceded with \'_\'\n"
+         << "\t  ex. _pi\n"
+         << "\t- every function needs to be preceded with \'?\'\n"
+         << "\t  ex. ?sin(_pi)\n"
+         << "\t- operators can be combined in any order and quantity, -* = ++*---\n"
+         << "\t- for use of brackets use only (), they can be freely nested\n"
+         << "\t- in floating point decimals use \'.\' as separator\n"
+         << "\t- in functions with many arguments separate them with \',\'\n"
+         << "\n"
+
+         << "Implemented constants:\n"
+         << "pi, e\n"
+         << "Implemented functions\n"
+         << "sin\n"
+
+         << "\n";
 }
 
 /*
